0e54871e2564b709b4bcdff9d95fab927e6ddb5c
A-Prathamesh1 <Prathamesh.ausekar1@gmail.com>
2018-12-21 01:57:12 -0800

    added Welcome prompt, chaging DOM using javascript

--- test website/scripts/main.js
+++ test website/scripts/main.js
@@ -1,2 +1,8 @@
 var myHeading = document.querySelector('h1');
 myHeading.textContent = 'Hello world!';
+
+var welcome = prompt("WELCOME TO THE WORLD OF JAVASCRIPT")
+//java script array objects
+var  cars = ["Tesla","Mercedez","Lamborgini"];
+document.getElementById("123").disabled = false;
+document.getElementById("123").innerHTML = cars[0];
