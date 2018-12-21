81042503c63b9d4c869a286412da22edba1d083d
A-Prathamesh1 <Prathamesh.ausekar1@gmail.com>
2018-12-21 02:12:44 -0800

    regressing

--- test website/scripts/main.js
+++ test website/scripts/main.js
@@ -4,5 +4,5 @@ myHeading.textContent = 'Hello world!';
 var welcome = prompt("WELCOME TO THE WORLD OF JAVASCRIPT")
 //java script array objects
 var  cars = ["Tesla","Mercedez","Lamborgini"];
-//document.getElementById("123").disabled = true;
+
 document.getElementById("123").innerHTML = cars[0];
