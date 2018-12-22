52ff7914a4cd9a4ab735b975a3725c53b7b8a845
A-Prathamesh1 <Prathamesh.ausekar1@gmail.com>
2018-12-22 13:10:37 -0800

    testing alert

--- test website/scripts/main.js
+++ test website/scripts/main.js
@@ -1,7 +1,7 @@
 var myHeading = document.querySelector('h1');
 myHeading.textContent = 'Hello world!';
-
-var welcome = prompt("WELCOME TO THE WORLD OF JAVASCRIPT")
+//var welcome =
+ prompt("WELCOME TO THE WORLD OF JAVASCRIPT")
 //java script array objects
 var  cars = ["Tesla","Mercedez","Lamborgini"];
 
