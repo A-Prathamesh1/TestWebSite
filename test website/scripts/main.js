79edbea776bee1fcce720d80f00d24734ddd31a7
A-Prathamesh1 <Prathamesh.ausekar1@gmail.com>
2019-01-02 21:52:35 -0800

    putting more array elements

--- test website/scripts/main.js
+++ test website/scripts/main.js
@@ -7,4 +7,7 @@ myHeading.textContent = 'Hello world!';
  prompt("WELCOME TO THE WORLD OF JAVASCRIPT")
 //java script array objects
 var  cars = ["Tesla","Mercedez","Lamborgini"];
+
 document.getElementById("test1").innerHTML = cars[0];
+document.getElementById("test2").innerHTML = cars[1];
+document.getElementById("test3").innerHTML = cars[2];
