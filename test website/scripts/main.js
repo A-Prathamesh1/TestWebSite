0851c303d2f68da31a79a3fbac16360943e6f064
A-Prathamesh1 <Prathamesh.ausekar1@gmail.com>
2019-01-02 17:30:30 -0800

    javascript Clock

--- test website/scripts/main.js
+++ test website/scripts/main.js
@@ -1,8 +1,12 @@
 var myHeading = document.querySelector('h1');
+
+
+
 myHeading.textContent = 'Hello world!';
 //var welcome =
  prompt("WELCOME TO THE WORLD OF JAVASCRIPT")
 //java script array objects
 var  cars = ["Tesla","Mercedez","Lamborgini"];
-
-document.getElementById("123").innerHTML = cars[0];
+array.forEach(element => {
+    document.getElementById("123").innerHTML = cars[0];    
+});
