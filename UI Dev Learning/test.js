b0178c202b7182af25420bd7570208335f73065d
A-Prathamesh1 <Prathamesh.ausekar1@gmail.com>
2020-06-26 15:38:28 -0500

    UI DEV learning

---
+++ UI Dev Learning/test.js
@@ -0,0 +1,19 @@
+var x = 10;
+//data types
+Number;
+string;
+boolean;
+null; // absence of value, something that has to get its value yet
+undefined; // type as well as value
+// loosely typed language
+// Angular is client side language
+// no type safety
+// all server side languages are strongly typed language.
+
+// Types of pop ups
+//confirm()
+//alert()
+//prompt()
+
+//javascript objects
+//
